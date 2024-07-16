79a63e37a70e85a40051c02707da590be002c1bd
omar93939 <om.markovinovic@gmail.com>
2024-07-16 19:31:52 +0200

    Qwen20.5B

--- tune.py
+++ tune.py
@@ -6,7 +6,7 @@ dtype = None
 load_in_4bit = True
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-  model_name = "facebook/opt-350m",
+  model_name = "Qwen/Qwen2-0.5B-Instruct",
   max_seq_length = max_seq_length,
   dtype = dtype,
   load_in_4bit = load_in_4bit
