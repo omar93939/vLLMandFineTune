47584ceca61ea6f60d39f6881968807a52647470
omar93939 <om.markovinovic@gmail.com>
2024-07-17 01:48:23 +0200

    gptq test

--- tune.py
+++ tune.py
@@ -6,7 +6,7 @@ dtype = None
 load_in_4bit = True
 
 model, tokenizer = FastLanguageModel.from_pretrained(
-  model_name = "cognitivecomputations/dolphin-2.9.2-qwen2-7b",
+  model_name = "Qwen/Qwen2-7B-Instruct-GPTQ-Int4",
   max_seq_length = max_seq_length,
   dtype = dtype,
   load_in_4bit = load_in_4bit
